60b8bae1444e497d040c4c09d0ea0259216d07a1
Maciej Wojdyła <maciejwojdyla@10g.pl>
2021-06-20 23:18:35 +0200

    updates people, updates

--- js.js
+++ js.js
@@ -5,6 +5,44 @@ function change(lang){
             break;
         case 'EN':
             loc = "./src/Maciej Wojdyła_CV_ENG.pdf";
+            break;
+        case 'tabela':
+            loc = "./Tabela.html";
+            break;
+        case 'css1':
+            loc = "./16.04/index1.html";
+        break;
+        case 'css2':
+            loc = "./CSS/index1.html";
+        break;
+        case 'css3':
+            loc = "./CSS/index2.html";
+        break;
+        case 'css4':
+            loc = "./CSS/index3.html";
+        break;
+        case 'css5':
+            loc = "./cssuczelnia1/index.html";
+        break;
+        case 'js1':
+            loc = "./js1/index.html";
+        break;
+        case 'js2':
+            loc = "./js2/index.html";
+        break;
+        case 'js3':
+            loc = "./js3/index.html";
+        break;
+        case 'js4':
+            loc = "./js4/index.html";
+        break;
+        case 'div':
+            loc = "./23.04/index.html";
+        break;
+        case 'xml':
+            loc = "./xmlcw/index1.html";
+        break;
+
     }
     document.getElementById('frame').src = loc;
 }
